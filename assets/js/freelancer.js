f8cbf4e89771a344738a117edda8a0659f778345
Astariul <remondnicola@gmail.com>
2019-11-27 20:35:55 +0900

    Move navbar to Dash layout instead of raw HTML + Fix #17
    Fix display problems and move navbar from raw HTML to Dash Layout. This allow us to modify the content of the navbar on the fly, for later implementing a multi lingual website.

--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -40,7 +40,7 @@
     }
   };
   // Collapse now if page is not at top
-  navbarCollapse();
+  // navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
